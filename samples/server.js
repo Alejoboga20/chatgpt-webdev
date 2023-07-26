8dfa6f7ed3ebaa62bd5fe8616e4038e70f1824ec
Alejo Boga <alejoboga20@gmail.com>
2023-07-26 09:55:55 -0400

    setup project

--- samples/server.js
+++ samples/server.js
@@ -1,6 +1,4 @@
-
-
-const OPENIA_KEY = "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";
+const OPENIA_KEY = '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';
 
 const path = require('path');
 const express = require('express');
@@ -11,61 +9,58 @@ app.use(express.json()); // parse JSON requests
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.post('/api/chat', async (req, res) => {
-  const body = req.body;
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer sess-vpiF9RZH2qkkmfXscAZUS3nIFIVdSDzw00jMOxAC',
-    },
-    body: JSON.stringify({
-        ...body,
-        temperature: 0,
-        max_tokens: 100,
-        model: 'gpt-3.5-turbo',
-        stream: false
-    })
-  });
-  res.json(await response.json());
+	const body = req.body;
+	const response = await fetch('https://api.openai.com/v1/chat/completions', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer sess-vpiF9RZH2qkkmfXscAZUS3nIFIVdSDzw00jMOxAC',
+		},
+		body: JSON.stringify({
+			...body,
+			temperature: 0,
+			max_tokens: 100,
+			model: 'gpt-3.5-turbo',
+			stream: false,
+		}),
+	});
+	res.json(await response.json());
 });
 
-
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: OPENIA_KEY,
+	apiKey: OPENIA_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
 app.post('/api/general', async (req, res) => {
-  const body = req.body;
+	const body = req.body;
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: body.prompt}],
-  });
-  res.json(completion.data.choices[0].message.content);
+	const completion = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [{ role: 'user', content: body.prompt }],
+	});
+	res.json(completion.data.choices[0].message.content);
 });
 
 app.post('/api/image', async (req, res) => {
-  const body = req.body;
+	const body = req.body;
 
-  const response = await openai.createImage({
-    prompt: body.prompt,
-    n: 1,
-    size: "512x512",
-  });
-  image_url = response.data.data[0].url;  
-  res.json({"url": image_url});
-})
+	const response = await openai.createImage({
+		prompt: body.prompt,
+		n: 1,
+		size: '512x512',
+	});
+	image_url = response.data.data[0].url;
+	res.json({ url: image_url });
+});
 
 app.post('/api/recipe', async (req, res) => {
-  const body = req.body;
+	const body = req.body;
 
-  const prompt = `Give me a recipe with the list of ingredientes defined in the markup
+	const prompt = `Give me a recipe with the list of ingredientes defined in the markup
   <ingredients>${body.ingredients}</ingredients>
   You can take for granted other basic ingredients, such as salt, pepper and other condiments we usually find in a kitchen.  
   
@@ -107,29 +102,29 @@ If one step's description has a time declaration like 'for 30 minutes', add the 
         ]
     }
 ***
-`
+`;
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content: "You are a professional chef who recommends recipes bases on ingredients"
-      },
-      {
-        role: "user",
-        content: prompt
-      }
-    ],
-    temperature: 0.7,
-    max_tokens: 1000,
-    model: 'gpt-3.5-turbo',
-    stream: false
-  });
+	const completion = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a professional chef who recommends recipes bases on ingredients',
+			},
+			{
+				role: 'user',
+				content: prompt,
+			},
+		],
+		temperature: 0.7,
+		max_tokens: 1000,
+		model: 'gpt-3.5-turbo',
+		stream: false,
+	});
 
-  res.json({content: completion.data.choices[0].message.content});
+	res.json({ content: completion.data.choices[0].message.content });
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+	console.log('Server started on port 3000');
 });
